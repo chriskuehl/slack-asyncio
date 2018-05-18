fb18da85fe80559e2a72a6e625613a3a82119420
Chris Kuehl <ckuehl@ckuehl.me>
2018-05-17 19:06:27 -0700

    Add Python package boilerplate

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='slack-asyncio',
+    description='Slack bot and API library using asyncio',
+    url='https://github.com/chriskuehl/slack-asyncio',
+    version='0.0.0',
+
+    author='Chris Kuehl',
+    author_email='ckuehl@ckuehl.me',
+
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+
+    packages=find_packages(exclude=('tests*', 'testing*')),
+    install_requires=[
+
+    ],
+)
